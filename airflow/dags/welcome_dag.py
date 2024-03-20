d3aff37b2dae58c86b82746688bbe0761c83c63b
raphael <salak.raphael@heb.com>
2024-03-20 13:33:06 -0500

    added call to asteroid api

--- airflow/dags/welcome_dag.py
+++ airflow/dags/welcome_dag.py
@@ -1,5 +1,44 @@
+import requests
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class APICallOperator(BaseOperator):
+    """
+    Custom operator to call an API with an API key included in the URL.
+    """
+
+    @apply_defaults
+    def __init__(self, api_url, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.api_url = api_url
+
+    def execute(self, context):
+        data_path = "/opt/airflow/dags/files/employees.csv"
+        self.log.info(f"Calling API: {self.api_url}")
+
+        try:
+            # Send a GET request to the API URL
+            response = requests.get(self.api_url)
+            with open(data_path, "w") as file:
+                file.write(response.text)
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                print("Data fetched from the API:")
+                print(data)
+                return data
+            else:
+                # If the request was not successful, log the error status code
+                self.log.error(f"Error calling API: {response.status_code}")
+                return None
+        except Exception as e:
+            # Handle any exceptions that occur during the request
+            self.log.error(f"Error calling API: {e}")
+            return None
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
 import requests
@@ -19,6 +58,7 @@ database = os.environ.get('database')
 username = os.environ.get('username')
 pwd = os.environ.get('password')
 port_id = os.environ.get('port_id')
+api_key = os.environ.get('api_key')
 
 def get_market_data():
     # No keys required for crypto data
@@ -36,18 +76,16 @@ def get_market_data():
 
     # Convert to dataframe
     print(btc_bars.df)
+def get_asteroid_data():
+
+    pass
 def transform_data():
     pass
 def connect_to_db():
     conn = None
     cur = None
     try:
-        conn = psycopg2.connect(
-                    host = hostname,
-                    dbname = database,
-                    user = username,
-                    password = pwd,
-                    port = port_id)
+        conn = psycopg2.connect(host = hostname, dbname = database, user = username, password = pwd, port = port_id)
         #cur = conn.cursor()
         print("successfully connected to postgres db")
     except Exception as error:
@@ -94,5 +132,12 @@ connect_to_db_task = PythonOperator(
     python_callable=connect_to_db,
     dag=dag
 )
+start_task = DummyOperator(task_id='start', dag=dag)
+api_call_task = APICallOperator(
+    task_id='api_call_task',
+    api_url='https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=' + str(api_key),
+    dag=dag,
+)
+end_task = DummyOperator(task_id='end', dag=dag)
 
-print_welcome_task >> print_hello_world_task >> get_market_data_task >> connect_to_db_task
+start_task >> api_call_task >> end_task
