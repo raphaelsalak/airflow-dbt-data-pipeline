4e7789d8536ef37a4011916f8ad8b3167814580e
raphael <salak.raphael@heb.com>
2024-04-25 20:33:49 -0500

    updated images

--- custom_postgres/models/asteroids/potentially_hazardous_asteroids_model.sql
+++ custom_postgres/models/asteroids/potentially_hazardous_asteroids_model.sql
@@ -1,11 +1,11 @@
-SELECT 'Hazardous asteroids' AS is_potentially_hazardous,
+SELECT 'Non-hazardous asteroids' AS is_potentially_hazardous,
        COUNT(*) AS pha_count
 FROM asteroid
-WHERE is_potentially_hazardous = 'true'
+WHERE is_potentially_hazardous = 'false'
 
 UNION ALL
 
-SELECT 'Non-dangerous asteroids' AS is_potentially_hazardous,
+SELECT 'Hazardous asteroids' AS is_potentially_hazardous,
        COUNT(*) AS total_count
 FROM asteroid
-where is_potentially_hazardous = 'false'
+where is_potentially_hazardous = 'true'
